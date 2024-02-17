dfb6692a225c4789615bc420ed4f6ee2035a552f
Gilfoyle3301 <criptobes3301@gmail.com>
2024-02-17 22:30:41 +0300

    added unpack function v.0.1.0

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,40 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
+var ErrParse = errors.New("invalid parse digit")
+var next_number bool
+var str_slice []string
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+
+	for i, v := range []rune(s) {
+		if i < len(s)-1 {
+			next_number = !unicode.IsDigit(rune(s[i+1]))
+		}
+		switch {
+		case unicode.IsLetter(v):
+			str_slice = append(str_slice, string(v))
+		case unicode.IsSpace(v):
+			str_slice = append(str_slice, strings.Trim(strconv.QuoteRune(v), "'"))
+		case unicode.IsDigit(v) && i != 0 && next_number:
+			digit, err := strconv.Atoi(string(v))
+			if err != nil {
+				return "", ErrParse
+			}
+			if digit == 0 {
+				str_slice = str_slice[:len(str_slice)-1]
+			} else {
+				str_slice = append(str_slice, strings.Repeat(str_slice[i-1], digit-1))
+			}
+		default:
+			return "", ErrInvalidString
+		}
+	}
+	return strings.Join(str_slice, ""), nil
 }
