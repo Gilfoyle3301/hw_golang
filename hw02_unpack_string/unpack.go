804ccc1eef9eca0f820b2c30796b451b54a0a832
Gilfoyle3301 <criptobes3301@gmail.com>
2024-02-18 10:59:31 +0300

    fix name varible

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -11,31 +11,31 @@ var ErrInvalidString = errors.New("invalid string")
 var ErrParse = errors.New("invalid parse digit")
 
 func Unpack(s string) (string, error) {
-	var next_number bool
-	var str_slice []string
+	var nextNumber bool
+	var strSlice []string
 
 	for i, v := range []rune(s) {
 		if i < len(s)-1 {
-			next_number = !unicode.IsDigit(rune(s[i+1]))
+			nextNumber = !unicode.IsDigit(rune(s[i+1]))
 		}
 		switch {
 		case unicode.IsLetter(v):
-			str_slice = append(str_slice, string(v))
+			strSlice = append(strSlice, string(v))
 		case unicode.IsSpace(v):
-			str_slice = append(str_slice, strings.Trim(strconv.QuoteRune(v), "'"))
-		case unicode.IsDigit(v) && i != 0 && next_number:
+			strSlice = append(strSlice, strings.Trim(strconv.QuoteRune(v), "'"))
+		case unicode.IsDigit(v) && i != 0 && nextNumber:
 			digit, err := strconv.Atoi(string(v))
 			if err != nil {
 				return "", ErrParse
 			}
 			if digit == 0 {
-				str_slice = str_slice[:len(str_slice)-1]
+				strSlice = strSlice[:len(strSlice)-1]
 			} else {
-				str_slice = append(str_slice, strings.Repeat(str_slice[i-1], digit-1))
+				strSlice = append(strSlice, strings.Repeat(strSlice[i-1], digit-1))
 			}
 		default:
 			return "", ErrInvalidString
 		}
 	}
-	return strings.Join(str_slice, ""), nil
+	return strings.Join(strSlice, ""), nil
 }
