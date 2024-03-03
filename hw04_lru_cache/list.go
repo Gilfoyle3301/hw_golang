18f5b934d85879199419e3f7e482e2024beaab2d
Gilfoyle3301 <criptobes3301@gmail.com>
2024-03-03 16:19:41 +0300

    HW04 impl interface List. Testing fail

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -16,11 +16,93 @@ type ListItem struct {
 	Prev  *ListItem
 }
 
-type list struct {
-	List // Remove me after realization.
-	// Place your code here.
-}
-
 func NewList() List {
 	return new(list)
+}
+
+type list struct {
+	lenght       int
+	beginElement *ListItem
+	endElement   *ListItem
+}
+
+func (l *list) Len() int {
+	return l.lenght
+}
+
+func (l *list) Front() *ListItem {
+	return l.beginElement
+}
+
+func (l *list) Back() *ListItem {
+	return l.endElement
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	mod := &ListItem{Value: v, Next: l.beginElement, Prev: nil}
+
+	if l.Len() == 0 {
+		l.beginElement = mod
+		l.endElement = mod
+		l.lenght++
+		return l.beginElement
+	}
+
+	if l.Len() != 0 {
+		l.beginElement.Prev = mod
+		l.beginElement = mod
+		l.lenght++
+	}
+	return l.beginElement
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	mod := &ListItem{Value: v, Next: nil, Prev: l.endElement}
+
+	if l.Len() == 0 {
+		l.beginElement = mod
+		l.endElement = mod
+		l.lenght++
+		return l.endElement
+	}
+
+	if l.Len() != 0 {
+		l.endElement.Next = mod
+		l.endElement = mod
+		l.lenght++
+	}
+	return l.endElement
+}
+
+func (l *list) Remove(i *ListItem) {
+	switch {
+	case i.Prev == nil:
+		l.beginElement = l.beginElement.Next
+		l.beginElement.Prev = nil
+		l.lenght--
+	case i.Next == nil:
+		l.endElement = l.endElement.Prev
+		l.endElement.Next = nil
+		l.lenght--
+	default:
+		i.Prev.Next = i.Prev
+		i.Next.Prev = i.Next
+		l.lenght--
+	}
+}
+func (l *list) MoveToFront(i *ListItem) {
+	switch {
+	case i.Prev == nil:
+		return
+	case i.Next == nil:
+		l.endElement = l.endElement.Prev
+		l.endElement.Next = nil
+	default:
+		i.Next.Prev = i.Prev
+		i.Prev.Next = i.Next
+	}
+	i.Next = l.beginElement
+	i.Prev = nil
+	l.beginElement = i
+
 }
