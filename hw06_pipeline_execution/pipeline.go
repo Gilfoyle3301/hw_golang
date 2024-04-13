da6f7b85a089eee38224f56e20aeb59115fe0e60
Gilfoyle3301 <criptobes3301@gmail.com>
2024-04-13 21:18:48 +0300

    HW06 first resolve && added modification test

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,14 +1,38 @@
 package hw06pipelineexecution
 
 type (
-	In  = <-chan interface{}
-	Out = In
-	Bi  = chan interface{}
+	In    = <-chan interface{}
+	Out   = In
+	Bi    = chan interface{}
+	Stage = func(in In) (out Out)
 )
 
-type Stage func(in In) (out Out)
-
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	for i := 0; i < len(stages); i++ {
+		in = Worker(done, stages[i](in))
+	}
+	return in
+}
+
+func Worker(done In, out Out) Bi {
+	localOut := make(Bi)
+
+	go func() {
+		defer close(localOut)
+	loop:
+		for {
+			select {
+			case value, ok := <-out:
+				if ok {
+					localOut <- value
+				} else {
+					break loop
+				}
+			case <-done:
+				return
+			}
+		}
+	}()
+
+	return localOut
 }
