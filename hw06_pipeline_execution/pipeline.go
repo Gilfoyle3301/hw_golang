5ce71db6b43260b5dc296f1cf47ab9cd51f2c41b
Gilfoyle3301 <criptobes3301@gmail.com>
2024-04-14 13:58:55 +0300

    HW06 change worker & loop

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,31 +8,24 @@ type (
 )
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	for i := 0; i < len(stages); i++ {
-		in = Worker(done, stages[i](in))
+	for _, stage := range stages {
+		in = Worker(done, stage(in))
 	}
 	return in
 }
 
-func Worker(done In, out Out) Bi {
-	localOut := make(Bi)
-
+func Worker(done In, in In) Bi {
+	out := make(Bi)
 	go func() {
-		defer close(localOut)
-	loop:
-		for {
+		defer close(out)
+		for element := range in {
 			select {
-			case value, ok := <-out:
-				if ok {
-					localOut <- value
-				} else {
-					break loop
-				}
 			case <-done:
 				return
+			default:
+				out <- element
 			}
 		}
 	}()
-
-	return localOut
+	return out
 }
